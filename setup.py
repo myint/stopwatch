789ee8bf83d4eb7b1b76b58003dc1fb575dbe51b
Steven Myint <git@stevenmyint.com>
2013-03-08 08:44:15 -0800

    Add installer

---
+++ setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+"""Setup for stopwatch."""
+
+from distutils import core
+
+
+core.setup(name='stopwatch',
+           scripts=['stopwatch'])
--- stopwatch.py
+++ stopwatch.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python
 # stopwatch is a pygame based stopwatch
 # Copyright (C) 2011 Alejandro Varas
 # based on code taken from http://www.bonf.net/2007/05/18/so/
